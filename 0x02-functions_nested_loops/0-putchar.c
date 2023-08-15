7f20f213aa8655977e55ca17c92cce6d7849d923
David <millerdavid0814@gmail.com>
2023-08-15 10:14:29 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - print _putchar
- * is written by David
+ * is written by mbah
  * Return: 0
  */
 
@@ -15,7 +15,7 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar(*\n');
+_putchar('\n');
 return (0);
 }
 
