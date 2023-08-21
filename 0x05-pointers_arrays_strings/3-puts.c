ed73cbb7937fcdeadd0da7e6748473cfaf017f04
David <millerdavid0814@gmail.com>
2023-08-21 04:51:05 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,17 @@
-#include "main.h"
-/**
- * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
- */
-void _puts(char *str)
-{
-	while (*str != '/0')
-	{
-		_putchar(*str++);
-	}
-		_putchar('\n');
-}
+#include <stdio.h>
 
+void _puts(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Holberton!";
+	_puts(str);
+	return (0);
+}
