3f67c26c67160c81838ec115b887ba6d4c96c2e7
Tamunoib <millerdavid0814@gmail.com>
2023-12-08 12:27:34 -0800

    Dlist

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds new head node to dlist
+ * @head: address of pointer to current head 
+ *
+ * 
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,4 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - Deletes a node from a dlistint_t at a given
