7674bdd2c1a5a649c5f5705d5a6b6113d97b59d5
Tamunoib <millerdavid0814@gmail.com>
2023-12-11 23:03:44 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ * Return: subtracted final result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod of two integers
+ * @a: first int
+ * @b: second int
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * mul - multiply two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
