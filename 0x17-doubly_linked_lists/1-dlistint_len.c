4ce4e44ef874364a266fa7017ea1a6067a46830f
Tamunoib <millerdavid0814@gmail.com>
2024-04-29 03:29:06 -0700

    double

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,20 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns length of dlist
- * @h: address of head node
+ * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ * @h: The head of the dlistint_t list.
  *
- * Return: size of list
+ * Return: The number of elements in the dlistint_t list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-        size_t i = 0;
+	size_t nodes = 0;
 
-        while (h)
-        {
-                h = h->next;
-                i++;
-        }
-        return (i);
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
